4e0dc46e676d2deb8b1dc68483ccbe61fa088431
Neetu <sahneetu754@gmail.com>
2025-09-27 23:44:01 +0530

    minore changes related to db

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const db = require("./dbConnection/db");
+// const db = require("./dbConnection/db");
 const cors = require("cors")
 
 // middleware
